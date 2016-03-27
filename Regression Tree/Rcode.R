2f42a2b3f30c294fd08c58ad191667346b1459f7
Aditya <mahajanaditya007@gmail.com>
2016-03-27 10:24:43 -0500

    updated the regression tree for the salary data

--- Regression Tree/Rcode.R
+++ Regression Tree/Rcode.R
@@ -1,39 +1,49 @@
+fullData <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", header=F) #import
+
+names(fullData) <- c("age", "workclass", "fnlwgt", "education", "educationnum", "maritalstatus", "occupation", "relationship", "race", "sex", "capitalgain", "capitalloss", "hoursperweek", "nativecountry", "response")
+
+fullData <- fullData[, c(15, 1:13)] # remove a factor with more than 31 levels.
+
+set.seed(100)
+
+train <- sample (1:nrow(fullData), .8*nrow(fullData)) # training row indices
+
+inputData <- fullData[train, ] # training data
+
+testData <- fullData[-train, ] # test data
+
 library(tree)
 
-fullData = read.csv("/home/aditya/Desktop/Aditya/Semper/Assignment 8/adult.data", header=F) #import
-
-names(fullData) = c("age", "workclass", "fnlwgt", "education", "educationnum", "maritalstatus", "occupation", "relationship", "race", "sex", "capitalgain", "capitalloss", "hoursperweek", "nativecountry", "response")
-
-fullData = fullData[, c(15, 1:13)] # remove a factor with more than 31 levels.
-
-
-train = sample (1:nrow(fullData), .8*nrow(fullData)) # training row indices
-
-inputData = fullData[train, ] # training data
-
-testData = fullData[-train, ] # test data
-
-
-treeMod = tree(response ~ ., data = inputData)  # model the tree, including all the variables
+treeMod <- tree(response ~ ., data = inputData)  # model the tree, including all the variables
 
 plot(treeMod)  # Plot the tree model
 
 text(treeMod, pretty=0)  # Add text to the plot
 
-cv.Model=cv.tree(treeMod) #running cross validation on the tree data
+out <- predict(treeMod) # Predict the training data
 
-plot(cv.Model$size,cv.Model$dev,type='b')# plotting the cross validated tree
+input.response <- as.character(inputData$response) # actuals
 
-prune.model=prune.tree(treeMod,best = 8)# pruning the tree with 8 terminal nodes
+pred.response <- colnames(out)[max.col(out, ties.method = c("first"))] # predicted
 
-plot(prune.model)# plotting the pruned tree
+cvTree <- cv.tree(treeMod, FUN = prune.misclass)  # run the cross validation
 
-text(prune.model,pretty = 0)
+plot(cvTree)  # plot the CV
 
-yhat=predict(treeMod,newdata = fullData[-train,])# predicting the values on testing data
+treePrunedMod <- prune.misclass(treeMod, best = 9) # set size corresponding to lowest value in below plot. try 4 or 16.
 
-fulldata.test=fullData[-train,'response']# getting the response for the testing data
+plot(treePrunedMod)
 
-pred.response = colnames(yhat)[max.col(yhat, ties.method = c("first"))] # predicted
+text(treePrunedMod, pretty = 0)
 
-mean(pred.response!=fulldata.test)
+out <- predict(treePrunedMod) # fit the pruned tree
+
+pred.response <- colnames(out)[max.col(out, ties.method = c("random"))] # predicted
+
+mean(inputData$response != pred.response) # Calculate Mis-classification error.
+
+testout <- predict(treePrunedMod, testData)  # Predict testData with Pruned tree
+
+predtest.response <- colnames(testout)[max.col(out, ties.method = c("random"))] # predicted
+
+mean(testData$response != predtest.response) # Calculate Mis-classification error.
